3302916436f224ba3f91646750c179021abd8491
anhld2998 <162439751+anhld2998@users.noreply.github.com>
2024-03-08 09:23:53 +0700

    commit

--- public/assets/js/research.js
+++ public/assets/js/research.js
@@ -18,6 +18,8 @@ document.addEventListener("DOMContentLoaded", function () {
       const query = searchInput.value.trim();
       if (query !== "") {
         search(query);
+      }else {
+        searchResults.innerHTML = "";
       }
     }, 300);
   });
@@ -41,28 +43,32 @@ document.addEventListener("DOMContentLoaded", function () {
       videos.forEach((video, index) => {
         // Tạo card cho kết quả
         const videoCard = document.createElement("div");
-        videoCard.classList.add("col-12 col-md-4", "mb-3");
-        videoCard.innerHTML = `  <div class="card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col-md-6 d-flex align-items-stretch">
-                    <div class="embed-responsive embed-responsive-16by9">
-                      <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${video.id.videoId}" allowfullscreen></iframe>
-                    </div>
+        videoCard.classList.add("col-md-6", "col-12", "mb-3");
+        videoCard.innerHTML = ` 
+        <div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-6 d-flex align-items-stretch justify-content-center">
+                <div class="embed-responsive embed-responsive-16by9  ">
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/${video.id.videoId}" allowfullscreen></iframe>
                 </div>
-                <div class="col-md-6">
-                    <div class="d-flex flex-column h-100">
-                        <h5 class="d-flex justify-content-start align-items-start card-title mb-0 limit-lines w-100">${video.snippet.title}</h5>
-                        <div class="mt-auto">
-                            <button class="btn btn-md btn-primary">
-                                Convert MP3
-                            </button>
-                        </div>
+            </div>
+            <div class="col-md-6">
+                <div class="d-flex flex-column h-100">
+                    <div class="title-container mt-2">
+                        <h6 class="card-title mb-0 w-100 h-100">${video.snippet.title}</h6>
+                    </div>
+                    <div class="ms-auto mt-2">
+                        <button class="btn btn-md btn-primary">
+                            Convert MP3
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>`;
+    </div>
+</div>
+    `;
         // Thêm card vào container hiện tại
         searchResultsContainer.appendChild(videoCard);
       });
